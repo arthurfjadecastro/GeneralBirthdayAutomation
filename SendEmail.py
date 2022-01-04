562a75ae45d0c24e6e15fa992e66aed4e6b183c2
arthur <arthurfjadecastro@gmail.com>
2022-01-04 20:33:48 -0300

    Polishments on fonts

--- SendEmail.py
+++ SendEmail.py
@@ -1,3 +1,4 @@
+from random import randint
 import win32com.client as client
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
@@ -7,6 +8,10 @@ import datetime
 
 da = datetime.date.today().strftime("%d/%m/%Y")
 outlook = client.Dispatch("Outlook.Application")
+
+phrase = [", hoje é um dia muito especial, pois você completa mais um ano de vida. Curta muito o seu aniversário ao lado das pessoas que mais ama. Felicidades!",
+          ", espero que hoje você celebre seu dia especial junto dos que mais ama, e que o seu coração se aqueça com todo amor que receber. Feliz aniversário!",
+          ", este é seu dia, e por isso deve festejar com alegria. Espero que receba muito carinho, homenagens e surpresas boas. Parabéns e muitas felicidades!"]
 
 df = pd.read_excel(
     r"C:\Users\Ravin\Desktop\send_email_python\Empregados.xlsx")
@@ -33,22 +38,29 @@ while index < len(data_list):
     message.To = data_list[index]["mat"]
     message.BCC = "marciano.matos@caixa.gov.br"
     message.Subject = "Feliz Aniversário!"
+    firstName = data_list[index]["name"].split()
+
     # Transformar primeiro nome
     # # Usar Matrícula em um while como destinatário
     # # Converter data de nascimento em Brasil e verificar se há necessidade de enviar o e-mail de aniversário
     my_image = Image.open(
         "C:\\Users\\Ravin\\Desktop\\send_email_python\\parabensind.jpg")
-    title_text = data_list[index]["name"] + \
-        ", este é seu dia, e por isso deve festejar com alegria. Espero que receba muito carinho, homenagens e surpresas boas. Parabéns e muitas felicidades!"
-    lines = textwrap.wrap(title_text, width=44)
+
+    title_text = firstName[0].capitalize() + \
+        phrase[randint(0, 2)]
+
+    print(title_text)
+    lines = textwrap.wrap(title_text, width=36)
     y_text = 100
     font = ImageFont.truetype(
         'C:\\Users\\Ravin\\Desktop\\send_email_python\\BebasNeue-Regular.ttf', 32)
+
     image_editable = ImageDraw.Draw(my_image)
+
     for line in lines:
         width, height = font.getsize(line)
-        image_editable.text(((550 - width) / 3, y_text),
-                            line, font=font, fill="white")
+        image_editable.text(((650 - width) / 3, y_text),
+                            line, font=font, fill="white", stroke_width=1, stroke_fill="white")
         y_text += height
 
     my_image.save("result.jpg")
