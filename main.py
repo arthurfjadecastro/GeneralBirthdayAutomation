40f372044e541254d0885fced7f7758ebddc5e6d
arthurfjadecastro <arthur_ravin@hotmail.com>
2022-10-23 12:00:07 -0300

    Break lines on text when draw image

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import textwrap
 import pandas as pd
 import datetime
 
+
+def received(mats):
+    text = ""
+    for x in mats:
+        text += x
+    return text
+
+
 outlook = client.Dispatch("Outlook.Application")
 
 df = pd.read_excel(
@@ -15,54 +23,10 @@ df = df[['Nome', 'Data', 'Matrícula']]
 #
 data_list = []
 for i in range(len(df)):
-    data_list.append({"Matrícula": df['Matrícula'][i]})
+    data_list.append({"Matrícula": df['Matrícula'][i], "Nome": df["Nome"][i]})
 
-# print(data_list)
-# index = 0
-# my_image = Image.open(
-#     "images/coletivo.jpg")
-#
-# title_text = []
-# font = ImageFont.truetype(
-#     'BebasNeue-Regular.ttf', 16)
-#
-# fontWeekDay = ImageFont.truetype(
-#     'BebasNeue-Regular.ttf', 22)
-#
-# sortedList = sorted(data_list, key=lambda x: x['dayOfTheWeek'])
-#
-# title_text = ExistingDayOfTheWeek(sortedList)
-
-# fontAss = ImageFont.truetype(
-#     'BebasNeue-Regular.ttf', 18)
-#
-# image_editable = ImageDraw.Draw(my_image)
-#
-# i = 0
-# y = 175
-# width2, height2 = font.getsize(dayWeekText[0])
-
-
-# image_editable.text(((800 - width2) / 3, y-30),
-#                     dayWeekText[0], font=fontWeekDay, fill="white", stroke_width=1, stroke_fill="white", align="baseline")
-# while i < len(title_text):
-#
-#     lines = textwrap.wrap(title_text[i], width=400)
-#     for line in lines:
-#         width, height = font.getsize(line)
-#         print(len(title_text))
-#         image_editable.text(((800 - width) / 3, y),
-#                             line.title(), font=font, fill="white", stroke_width=0, stroke_fill="white",
-#                             align="baseline")
-#         print(((height * 1.5) / (len(title_text))))
-#         y += height * \
-#              2 if len(title_text) < 6 else (
-#             ((height * 19) / (len(title_text))))
-#     i = i + 1
-
-# my_image.save("result.png", optimize=True, quality=100)
 j = 0
-message = outlook.CreateItem(0)
+
 mats = []
 while j < len(data_list):
     mats.append(data_list[j]["Matrícula"])
@@ -75,16 +39,72 @@ while j < len(data_list):
 #     message.BCC = ass['Assinatura'][2]
 #     message.Subject = "Feliz Aniversário!"
 
-def received(mats):
-    text = ""
-    for x in mats:
-        text += x
-    return text
 
-a = received(mats)
+n = 0
+nam = []
+while n < len(data_list):
+    nam.append(data_list[n]["Nome"] + "\n")
+    n += 1
 
-message.To = a
-message.Display()
+matriculas = received(mats)
+names = received(nam)
+message = outlook.CreateItem(0)
+message.To = matriculas
+# message.Display()
+
+# index = 0
+
+# Get original image
+my_image = Image.open(
+    "C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\images\\coletivo.jpg")
+
+# Specific title fonts
+
+
+# Specific body fonts
+# fontWeekDay = ImageFont.truetype(
+#     'BebasNeue-Regular.ttf', 22)
+
+
+# Get image to design
+
+
+# Constants to alignment text in image
+i = 0
+y = 175
+# Define dimension fonts
+# width2, height2 = font.getsize(names)
+
+
+box = ((10, 10, 490, 190))
+image_editable = ImageDraw.Draw(my_image)
+image_editable.rectangle(box, outline="#000")
+
+# names = "This is some\nexample text"
+font_size = 100
+size = None
+while (size is None or size[0] > box[2] - box[0] or size[1] > box[3] - box[1]) and font_size > 0:
+    font = ImageFont.truetype(
+        'C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\fonts\\BebasNeue-Regular.ttf',
+        16)
+    size = font.getsize_multiline(names)
+    font_size -= 1
+    image_editable.multiline_text((box[0], box[1]), names, "#000", font)
+#
+# lines = ""
+# # Break lines text`s on image
+# while i < len(names):
+#     lines = textwrap.wrap(names[i], width=400)
+#     y += 100
+#     i = i + 1
+# # for line in lines:
+# #     width, height = font.getsize(line)
+# image_editable.text(((200) / 3, y - 30), names, font=font, fill="white", stroke_width=1, stroke_fill="white",
+#                     align="baseline")
+
+# Save image final result
+my_image.save("C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\images\\result.png",
+              optimize=True, quality=100)
 
 # html_body = """
 #     <div>
