dad136913a2525aa11374ea3100a9132642a64af
arthurfjadecastro <arthur_ravin@hotmail.com>
2022-10-23 20:09:54 -0300

    Insert draw image into body html outlook

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import win32com.client as client
 from PIL import Image, ImageFont, ImageDraw
-import textwrap
 import pandas as pd
-import datetime
+import os
+
+# Create Absolute Path
+file_path = os.path.abspath(os.path.dirname(__file__))
+absolutPath = "\"" + \
+              file_path.replace("\\", "\\\\") + "\\\\result.png" + "\""
 
 
 def received(mats):
@@ -18,12 +22,12 @@ df = pd.read_excel(
     r"C:\Users\Arthur\Desktop\analisar t\workspace\send_leyman_email_coletivo\sisrh\Empregados.xlsx",
     sheet_name='DataBase')
 
-df = df[['Nome', 'Data', 'Matrícula']]
+df = df[['Nome', 'Data', 'Matrícula', "Unidade"]]
 
 #
 data_list = []
 for i in range(len(df)):
-    data_list.append({"Matrícula": df['Matrícula'][i], "Nome": df["Nome"][i]})
+    data_list.append({"Matrícula": df['Matrícula'][i], "Nome": df["Nome"][i], "Unidade": df["Unidade"][i]})
 
 j = 0
 
@@ -36,85 +40,57 @@ while j < len(data_list):
     # while t < len(data_list):
 
     # t += 1
-#     message.BCC = ass['Assinatura'][2]
-#     message.Subject = "Feliz Aniversário!"
-
+    #     message.BCC = ass['Assinatura'][2]
 
 n = 0
 nam = []
 while n < len(data_list):
-    nam.append(data_list[n]["Nome"] + "\n")
+    nam.append(data_list[n]["Nome"] + "  -  " + data_list[n]["Unidade"] + "\n\n")
     n += 1
 
 matriculas = received(mats)
 names = received(nam)
 message = outlook.CreateItem(0)
 message.To = matriculas
-# message.Display()
-
-# index = 0
+message.Subject = "Feliz Aniversário - Parabenize seus colegas! 🎉🎈🎁"
+message.Display()
 
 # Get original image
 my_image = Image.open(
     "C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\images\\coletivo.jpg")
 
-# Specific title fonts
-
-
-# Specific body fonts
-# fontWeekDay = ImageFont.truetype(
-#     'BebasNeue-Regular.ttf', 22)
-
-
-# Get image to design
-
-
-# Constants to alignment text in image
-i = 0
-y = 175
-# Define dimension fonts
-# width2, height2 = font.getsize(names)
-
-
-box = ((10, 10, 490, 190))
+box = ((100, 175, 490, 400))
 image_editable = ImageDraw.Draw(my_image)
-image_editable.rectangle(box, outline="#000")
+# image_editable.rectangle(box)
 
 # names = "This is some\nexample text"
 font_size = 100
 size = None
+
+print(names)
 while (size is None or size[0] > box[2] - box[0] or size[1] > box[3] - box[1]) and font_size > 0:
     font = ImageFont.truetype(
         'C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\fonts\\BebasNeue-Regular.ttf',
-        16)
+        14)
     size = font.getsize_multiline(names)
     font_size -= 1
-    image_editable.multiline_text((box[0], box[1]), names, "#000", font)
-#
-# lines = ""
-# # Break lines text`s on image
-# while i < len(names):
-#     lines = textwrap.wrap(names[i], width=400)
-#     y += 100
-#     i = i + 1
-# # for line in lines:
-# #     width, height = font.getsize(line)
-# image_editable.text(((200) / 3, y - 30), names, font=font, fill="white", stroke_width=1, stroke_fill="white",
-#                     align="baseline")
+    image_editable.multiline_text((box[0], box[1]), names, font=font, align="center", fill="white", stroke_fill="white",
+                                  spacing=2)
 
 # Save image final result
-my_image.save("C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\images\\result.png",
+my_image.save("C:\\Users\\Arthur\\Desktop\\analisar t\\workspace\\send_leyman_email_coletivo\\result.png",
               optimize=True, quality=100)
+
+html_body = f"""
+           <div>
+               <img src={absolutPath}>
+           </div> 
+           """
+message.HTMLBody = html_body
 
 # html_body = """
 #     <div>
-#         <img src="result.png">
+#         OI
 #     </div>
 #     """
 
-html_body = """
-    <div>
-        OI
-    </div>
-    """
-message.HTMLBody = html_body
